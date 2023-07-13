38f410fe505723f7d5e00418862496516cb0fbe5
FilipeTheAnalyst <fbalseiro@gmail.com>
2023-07-13 23:54:30 +0000

    update to read only updated last 1 hour

--- gcs_read.py
+++ gcs_read.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pandas_gbq
 from google.cloud import storage
+from datetime import datetime, timedelta, timezone
 
 def read_json_from_gcs(bucket_name, file_path):
     # Initialize Google Cloud Storage client
@@ -12,37 +13,50 @@ def read_json_from_gcs(bucket_name, file_path):
     # List all files in the specified file path
     blobs = bucket.list_blobs(prefix=file_path)
 
+   # Get the datetime for one hour ago in the local timezone
+    one_hour_ago = datetime.now() - timedelta(hours=1)
+    one_hour_ago = one_hour_ago.replace(tzinfo=timezone.utc).astimezone()
+
     # Initialize an empty list to store the merged DataFrames
     merged_dfs = []
 
     # Iterate over each JSON file
     for blob in blobs:
-        # Download the JSON file from GCS to a string
-        json_data = blob.download_as_text()
+        # Get the last modified timestamp of the file
+        last_modified = blob.updated
 
-        # Convert the JSON data to a DataFrame
-        df = pd.read_json(json_data, lines=True)
+        # Check if the file was last modified during the last hour
+        if last_modified >= one_hour_ago:
+            # Download the JSON file from GCS to a string
+            json_data = blob.download_as_text()
 
-        # Normalize the 'data' field based on the nested keys
-        nested_columns = ['data.id', 'data.location']
-        df_normalized = pd.json_normalize(df['data'])
+            # Convert the JSON data to a DataFrame
+            df = pd.read_json(json_data, lines=True)
 
-        # Merge the normalized DataFrame with the original DataFrame
-        df_merged = pd.concat([df, df_normalized], axis=1)
+            # Normalize the 'data' field based on the nested keys
+            nested_columns = ['data.id', 'data.location']
+            df_normalized = pd.json_normalize(df['data'])
 
-        # Drop the 'data' column
-        df_merged = df_merged.drop(columns=['data'])
+            # Merge the normalized DataFrame with the original DataFrame
+            df_merged = pd.concat([df, df_normalized], axis=1)
 
-        # Append the merged DataFrame to the list
-        merged_dfs.append(df_merged)
+            # Drop the 'data' column
+            df_merged = df_merged.drop(columns=['data'])
 
-    # Concatenate all the merged DataFrames
-    merged_df = pd.concat(merged_dfs, ignore_index=True)
+            # Append the merged DataFrame to the list
+            merged_dfs.append(df_merged)
 
-    # Change the column names if needed
-    merged_df = merged_df.rename(columns={'location.lat': 'location_lat', 'location.lng': 'location_lng', 'location.at': 'location_at'})
+    if merged_dfs:
+        # Concatenate all the merged DataFrames
+        merged_df = pd.concat(merged_dfs, ignore_index=True)
 
-    return merged_df
+        # Change the column names if needed
+        merged_df = merged_df.rename(columns={'at': 'created_at', 'location.lat': 'location_lat', 'location.lng': 'location_lng', 'location.at': 'location_created_at'})
+
+        return merged_df
+    else:
+        print("No JSON files found within the last hour.")
+        return None
 
 def save_dataframe_to_bigquery(dataframe, dataset_name, project_id):
     table_name = f'{dataset_name}.raw_data'
@@ -57,5 +71,7 @@ if __name__ == '__main__':
     project_id = 'hallowed-name-392510'
     # Read JSON data from GCS for each file in the specified path and save the merged result to BigQuery
     merged_df = read_json_from_gcs(bucket_name, file_path)
-    save_dataframe_to_bigquery(merged_df, dataset_name, project_id)
+
+    print(merged_df)
+    # save_dataframe_to_bigquery(merged_df, dataset_name, project_id)
 
